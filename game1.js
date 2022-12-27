82485e576da8036e47d9bb1ca06bd23f3dbc56cc
galiaorden@outlook.com <galiaorden@outlook.com>
2022-12-27 22:29:51 +0200

    the memory game -ready

--- game1.js
+++ game1.js
@@ -1,23 +1,42 @@
 const cards = document.querySelectorAll(".card");
-
+const clock = document.querySelector(".timer b");
+const startGame = document.querySelector(".start-game");
 let card1, card2;
 let disable = false;
 let numOfMatches = 0;
+let timer;
+let leftFlips = 20;
 
 function flipingCard(e) {
   let clickCard = e.target;
   if (clickCard !== card1 && !disable) {
+    leftFlips--;
+
+    document.getElementById("left_flips").innerHTML = leftFlips;
+    //emphasizing in red that the left flips is going to end
+    if (leftFlips <= 5) {
+      document.querySelector(".flips").classList.add("flips_red");
+    }
     clickCard.classList.add("flip");
     if (!card1) {
       return (card1 = clickCard);
     }
+    // for not fliping 3 cards at the same time
     disable = true;
     card2 = clickCard;
     let card1Img = card1.querySelector(".back-view img").src;
     console.log(card1Img);
     let card2Img = card2.querySelector(".back-view img").src;
     console.log(card2Img);
-    ceckMatch(card1Img, card2Img);
+    let ifWin = ceckMatch(card1Img, card2Img);
+    //check if there is a lose in the game
+    if (leftFlips === 0 && ifWin !== 1) {
+      //stop the timer
+      clearInterval(timer);
+      setTimeout(() => {
+        return endOfGame("You are such a loser😒");
+      }, 2500);
+    }
   }
 }
 //this func checks if there is a match between 2 cards
@@ -27,9 +46,14 @@ function ceckMatch(card1Img, card2Img) {
     numOfMatches++;
     if (numOfMatches === 10) {
       //end of the game
+      //stop the timer
+      clearInterval(timer);
       setTimeout(() => {
-        return finishCards();
-      }, 2000);
+        return endOfGame(
+          `You won the game!🤩<br> Time passed: ${clock.innerHTML} s`
+        );
+      }, 1000);
+      return 1;
     }
 
     card1.removeEventListener("click", flipingCard);
@@ -54,19 +78,70 @@ function finishCards() {
   numOfMatches = 0;
   disable = false;
   card1 = card2 = "";
+  leftFlips = 20;
+  document.getElementById("left_flips").innerHTML = leftFlips;
+  let message = document.getElementById("game-over");
+  message.classList.add("hide-message");
+  //if it turned to red last game return it to black
+  document.querySelector(".flips").classList.remove("flips_red");
+  for (let i = 0; i < cards.length; i++) {
+    cards[i].classList.remove("flip");
+  }
   //creat an array for diffrent order of cards
   let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   arr.sort(() => (Math.random() > 0.5 ? 1 : -1));
-  for (let i = 0; i < cards.length; i++) {
-    cards[i].classList.remove("flip");
-    let imgTag = cards[i].querySelector(".back-view img");
-    imgTag.src = `flags_pic/flag${arr[i]}.png`;
-    cards[i].addEventListener("click", flipingCard);
-  }
+  //the exrtra time is for not seeing the next card before it finish to flip the side
+  setTimeout(() => {
+    for (let i = 0; i < cards.length; i++) {
+      // cards[i].classList.remove("flip");
+      let imgTag = cards[i].querySelector(".back-view img");
+      imgTag.src = `flags_pic/flag${arr[i]}.png`;
+      cards[i].addEventListener("click", flipingCard);
+    }
+  }, 500);
+  //start the timer
+
+  timeOver();
 }
-finishCards();
+
+function endOfGame(str) {
+  disable = true; //unable to flip the cards
+  document.querySelector("#game-over b").innerHTML = str;
+
+  let message = document.getElementById("game-over");
+  message.classList.remove("hide-message");
+  let startGame = document.querySelector(".start-game");
+  startGame.addEventListener("click", finishCards);
+}
+function refreshing() {
+  //stop the timer
+  clearInterval(timer);
+  startGame.innerHTML = "start again";
+  return endOfGame("Do you want to start again?🙂");
+}
+//this function calculate the timer of the game
+function timeOver() {
+  let sec = 1;
+  let minute = 0;
+
+  timer = setInterval(() => {
+    if (sec <= 9) {
+      clock.innerHTML = minute + ":0" + sec;
+    } else if (sec <= 59) {
+      clock.innerHTML = minute + ":" + sec;
+    } else {
+      //there is more than 60 seconds
+      minute++;
+      sec -= 60;
+      clock.innerHTML = minute + ":00";
+    }
+    sec++;
+  }, 1000);
+}
+
+endOfGame("Let's start the game!😁");
 
 for (let i = 0; i < cards.length; i++) {
-  //cards[i].classList.add("flip");
   cards[i].addEventListener("click", flipingCard);
 }
+document.getElementById("refresh").addEventListener("click", refreshing);
