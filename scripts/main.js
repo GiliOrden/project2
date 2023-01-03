73f1a381b438d80d8a38423797917f8111765c46
galiaorden@outlook.com <galiaorden@outlook.com>
2023-01-03 13:05:53 +0200

    a little change

--- scripts/main.js
+++ scripts/main.js
@@ -2,12 +2,11 @@ if (!localStorage.user) {
   let links = document.querySelectorAll("a.game");
   for (let i = 0; i < links.length; i++) {
     links[i].onclick = need_log_in;
-    // links[i].classList.add("a_hide");
     links[i].href = "";
   }
 }
 function need_log_in() {
-  const massage = document.getElementById("game-over");
+  const massage = document.getElementById("need_log_in");
   massage.classList.remove("hide-message");
   return false;
 }
