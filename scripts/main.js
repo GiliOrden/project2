cda931ffd51f0599216f261ea5cd5bc4151fb508
galiaorden@outlook.com <galiaorden@outlook.com>
2023-01-03 12:51:11 +0200

    main change

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,10 @@ if (!localStorage.user) {
   let links = document.querySelectorAll("a.game");
   for (let i = 0; i < links.length; i++) {
     links[i].href = "";
-    links[i].onclick = () => false;
+    links[i].onclick = need_log_in;
   }
+}
+function need_log_in() {
   const massage = document.getElementById("game-over");
-  //   massage.classList.remove("hide-message");
+  massage.classList.remove("hide-message");
 }
