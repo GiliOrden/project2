bb86b8cfd2647f0e7084d4523498f8a973f0aa90
galiaorden@outlook.com <galiaorden@outlook.com>
2023-01-03 12:47:29 +0200

    some changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
 if (!localStorage.user) {
-    let links = document.querySelectorAll("a.game");
-    for (let i = 0; i < links.length; i++) {
-        links[i].href = "";
-    }
+  let links = document.querySelectorAll("a.game");
+  for (let i = 0; i < links.length; i++) {
+    links[i].href = "";
+  }
+  const massage = document.getElementById("game-over");
+  //   massage.classList.remove("hide-message");
 }
