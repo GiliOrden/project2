f0a5e831110fd239c54f69ffaf11db25eb128f6a
תכלת כהן <tccohen@g.jct.ac.il>
2023-01-03 11:55:10 +0200

    fix main

--- scripts/main.js
+++ scripts/main.js
@@ -2,5 +2,6 @@ if (!localStorage.user) {
     let links = document.querySelectorAll("a.game");
     for (let i = 0; i < links.length; i++) {
         links[i].href = "";
+        links[i].onclick = () => false;
     }
 }
